732b0d0898a49841e6a6586cba066f30e49ab5bc
Oktay Değerli <oktaydegerli@gmail.com>
2025-06-23 11:34:24 +0300

    Update climate.py

--- custom_components/tuya_ir_ac/climate.py
+++ custom_components/tuya_ir_ac/climate.py
@@ -238,9 +238,10 @@ class TuyaIrClimateEntity(ClimateEntity, RestoreEntity):
 
         try:
             if self._device_model is "MSZ-GE25VA-v2" or self._device_model is "MSC-GE35VB-v2":
-                head = "010ed8000000000005000f003500260045008c"
-                key = "001^%0070C4D364800024C0E04000000000A2@$"
-                await self._async_send_command({"201": json.dumps({"control": "send_ir", "head": head, "key1": key, "type": 0, "delay":300})})
+                await self._async_send_command({"1": "study_key", "7": codecs.encode(codecs.decode(ir_code, 'hex'), 'base64').decode()})
+                # head = "010ed8000000000005000f003500260045008c"
+                # key = "001^%0070C4D364800024C0E04000000000A2@$"
+                # await self._async_send_command({"201": json.dumps({"control": "send_ir", "head": head, "key1": key, "type": 0, "delay":300})})
             else:
                 await self._async_send_command({"1": "study_key", "7": codecs.encode(codecs.decode(ir_code, 'hex'), 'base64').decode()})
         except Exception as e:
